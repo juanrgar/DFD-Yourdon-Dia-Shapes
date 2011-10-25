31717aae1332e4a0abb8547b5e7c286eb018333d
Juan R. Garcia <juanrgar@gmail.com>
2011-10-25 23:19:55 +0200

    added more conn points to bubble

--- make-install.sh
+++ make-install.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 BASEURL="https://raw.github.com/juanrgar/DFD-Yourdon-Dia-Shapes/master"
-SHEET_DEST="$HOME/.dia/sheets/"
-SHAPES_DEST="$HOME/.dia/shapes/"
+
+DIA_HOME="$HOME/.dia"
+SHEET_DEST="$DIA_HOME/sheets/"
+SHAPES_DEST="$DIA_HOME/shapes/"
 
 SHEETS="dfd-yourdon.sheet"
 SHAPES="bubble.shape \
@@ -22,7 +24,7 @@ get_shapes()
     do
         SHAPE_PATH="$BASEURL/shapes/$shape"
         echo "Downloading $SHAPE_PATH ..."
-        curl $SHAPE_PATH -o "$SHAPES_DEST/$shape" || do_exit
+        curl --create-dirs $SHAPE_PATH -o "$SHAPES_DEST/$shape" || do_exit
     done
 }
 
@@ -32,9 +34,14 @@ get_sheet()
     do
         SHEET_PATH="$BASEURL/sheets/$sheet"
         echo "Downloading $SHEET_PATH ..."
-        curl $SHEET_PATH -o "$SHEET_DEST/$sheet" || do_exit
+        curl --create-dirs $SHEET_PATH -o "$SHEET_DEST/$sheet" || do_exit
     done
 }
+
+if [ ! -d $DIA_HOME ]; then
+    echo "Try to run Dia first"
+    do_exit
+fi
 
 get_shapes
 get_sheet
